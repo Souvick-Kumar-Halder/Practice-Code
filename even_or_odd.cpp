b6a088f20e8637f5e159aed5d47a1311533f2e70
Souvick Kumar Halder <140925579+Souvick-Kumar-Halder@users.noreply.github.com>
2024-07-15 19:22:16 +0530

    file uploaded

---
+++ even_or_odd.cpp
@@ -0,0 +1,18 @@
+//even or odd finder
+#include<iostream>
+using namespace std;
+int main(){
+
+int num;
+cout<<"Enter the number:";
+cin>>num;
+if(num%2==0){
+   cout<<"EVEN";
+}else{
+    cout<<"ODD";
+}
+
+
+
+    return 0;
+}
